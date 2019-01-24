a58c4920867085f6d4a05d35e3b4e50b00604849
erikhag1 <erik.hagen@difi.no>
2019-01-24 05:56:39 +0100

    Oppdatert standardheader

--- test/ARIS 10/exportedModels.js
+++
@@ -1,8 +0,0 @@
-var JS_Array_Entries_Of_Models = new Array('mb81b6940-1154-11e9-3e58-c85b7643609d','m664de5f0-1075-11e9-3e58-c85b7643609d','m68b59990-14c7-11e9-3e58-c85b7643609d','m987dba80-1077-11e9-3e58-c85b7643609d','mb10be090-1275-11e9-3e58-c85b7643609d','m94fdad30-1274-11e9-3e58-c85b7643609d','m37ac9450-12ad-11e9-3e58-c85b7643609d','m6af477d0-164d-11e9-3e58-c85b7643609d');function searchModel(model) {
-for (var i = 0; i < JS_Array_Entries_Of_Models.length; i++) {
-if (model == JS_Array_Entries_Of_Models[i]) {
-return true;
-}
-}
-return false;
-}
